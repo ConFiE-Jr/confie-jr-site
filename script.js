717b38d25537658f82c63609d6843a78e7029b67
ConFiE-Jr <confie.ej@gmail.com>
2024-10-20 15:38:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,42 @@ document.addEventListener('scroll', function() {
         }
     });
 });
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Função para mudar os slides
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+// Função para slides atuais
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    // Esconde todos os slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Remove a classe "active" de todos os pontos
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Exibe o slide atual e ativa o ponto correspondente
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
+}
